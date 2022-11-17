6bd69e9859858c78035ad88f88f001febf26df41
Mahy Clément <210305@CCM-Asty-Moulin.local>
2022-11-17 14:30:29 +0100

    .

---
+++ Mahy Clément Manipulation PHP/index2.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manipulation PHP</title>
+</head>
+<body>
+    <h1>Exercice 1</h1>
+    <?php for ($i = 1; $i <= 10; $i++) : ?>
+        <p>Le nombre vaut <?=$i?> </p>
+    <?php endfor ?>
+    
+    <h1>Exercice 2</h1>
+    <?php for ($i = 1; $i <= 10; $i++) : ?>
+        <?php if ($i != 3) : ?>
+            <p>Le nombre vaut <?=$i?></p>
+        <?php endif ?>
+    <?php endfor ?>
+    
+    <h1>Exercice 3</h1>
+    <?php for ($i = 1; $i <= 10; $i++) : ?>
+        <?php if ( $i<4 || $i>7) : ?>
+            <p>Le nombre vaut <?=$i?></p>
+        <?php endif ?>
+    <?php endfor ?>
+
+    <h1>Exercice 4</h1>
+    <?=$n1 = -5;?>
+    <?=$n2 = 10;?>
+    <?php 
+    
+    echo "<p>La valeur absolue de ".$n1." vaut ".abs($n1);
+    echo "<p>La valeur absolue de ".$n2." vaut ".abs($n2);
+
+</body>
+</html>
