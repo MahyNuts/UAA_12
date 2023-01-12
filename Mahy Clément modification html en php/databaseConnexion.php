527422686cf351262f6cf9c0eba3fbdc3d5b1f29
Mahy Clément <210305@CCM-Asty-Moulin.local>
2023-01-12 14:26:39 +0100

    .

---
+++ Mahy Clément modification html en php/databaseConnexion.php
@@ -0,0 +1,9 @@
+<?php
+    try{
+        $strConnection = ('mysql:host=10.10.51.98;dbname=immobilier;port=3306');
+        $pdo = new PDO($strConnection, "clement", "root");
+        die("Connexion réussie");
+    } catch (PDOException $e){
+        die("Erreur : " . $e->getMessage());
+    }
+?>
--- Mahy Clément modification html en php/index.php
+++ Mahy Clément modification html en php/index.php
@@ -1,3 +1,6 @@
+<?php
+    require_once "databaseConnexion.php";
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
